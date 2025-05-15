0b288d5e05c18c6dc08cf9f627e3127f37db8950
raghavagrandhi <raghava.grandhi@gmail.com>
2025-05-15 17:53:57 +0530

    Update streamlit_app.py
    Updated Version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -14,7 +13,9 @@ name_on_order = st.text_input("Name on Smoothie:", "")
 if name_on_order:
     st.write("The name on your Smoothie will be: ", name_on_order)
 
-session = get_active_session()
+cnx=st.connection("snowflake")
+#session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
